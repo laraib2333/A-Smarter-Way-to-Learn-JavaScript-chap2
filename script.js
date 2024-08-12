9ce980d75b1b1086a499444dfa40ade0251215b6
laraib2333 <laraibahmed2333@gamil.com>
2024-08-11 21:28:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1. Declare 3 variables in one statement.
+
+// var name,
+// birthYear,
+//  age;
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// // Legal variable names:
+
+// var laraib;
+// var $laraib;
+// var laraib232;
+// var laraib_ahmed;
+// var laraibAhmed;
+
+// // illegal variable names.
+
+// var laraib-ahmed;        We Cannot use Hyphens 
+// var 232laraib;               We Cannot start with a number
+// var laraib ahmed;         We Cannot add spaces 
+// var laraib@ahmed;      We Cannot use special characters
+// var var;                         We Cannot use reserved keyword
+
+
+// 3. Display this in your browser
+
+// document.write("<h3>Rules for naming JS variables</h3><br><br>                                 Variable names can only contain, numbers, $ and _. For example : $my_1stVariable                  <br>Variable must begin with a letter, $ or _ . For example : $name, _name or name             <br>Variable name are case sensitive                                                               <br>Variable names should not be JS keywords") ;
+                                
